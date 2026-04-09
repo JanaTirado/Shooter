4a67f0bd343794bbc82bdad07636434eebc3fec2
JanaTirado <hjna.tm@gmail.com>
2026-04-08 22:27:25 -0500

    siiii

--- DOOM/Assets/Scripts/Weapons.cs
+++ DOOM/Assets/Scripts/Weapons.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 
 [CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon/New Weapon")]
-public class WeaponSO : ScriptableObject
+public class Weapons : ScriptableObject
 
 {
     public float range;
